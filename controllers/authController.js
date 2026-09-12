27a999a09cf1dd14204fe4dfdef3be6002f8b454
Abhishek Kumar <abhishek.kumar@speqto.com>
2026-09-12 14:47:47 +0530

    fixes error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 function createToken(userId) {
   return jwt.sign(
